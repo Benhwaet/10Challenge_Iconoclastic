982c9e101a48b1a10878a41480a9a6c6e9ef2b0e
BenHwaet <benmdesroches@gmail.com>
2023-08-21 21:42:17 -0300

    validate-color npm works, huzzah! now to create svg file accurately

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const promptUser = require('./lib/prompts.js');
 const generateSVG = require('./lib/generateSVG.js');
 const { writeFile } = require('fs');
 
+
 // function to generate SVG markup
-const init = () => {
+const init = async () => {
     promptUser()
-    .then ((answers) => writeFile('logo.svg', generateSVG(answers)))
-    .then(() => console.log('Successfully wrote to logo.svg'))
-    .catch((err) => console.error(err));
-    };
-    
-    //call to initialize app
-    init();
+    // to avoid a callback error, looked up solution on bobbyhadz.com, 
+    //can be written otherwise, will try this first
+        .then((answers) => writeFile('logo.svg', generateSVG(answers), err => {
+            if (err) {
+                console.log(err.message);
+                throw err;
+            }
+        console.log('Successfully wrote to logo.svg');
+    })
+)};
+
+
+
+//call to initialize app
+init();
